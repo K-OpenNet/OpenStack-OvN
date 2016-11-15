de4d088fc834451f5cd941389f231eac213b5cbf
Jun-Sik Shin <jsshin@smartx.kr>
2016-11-15 17:01:18 +0900

    modify parse.py

---
+++ infoelems.py
@@ -0,0 +1,20 @@
+class Info:
+    def __init__(self):
+        self.box = None
+        self.bridges = None
+        self.tunnels = None
+        self.flows = None
+
+
+
+class BoxInfoElem:
+    pass
+
+
+class BridgesInfoElem:
+    pass
+
+
+class TunnelsInfoElem:
+    pass
+
--- parse.py
+++ parse.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import yaml
+import logging
 
 
 class TemplateParser(object):
@@ -11,37 +12,55 @@ class TemplateParser(object):
         return cls._instance
 
     def __init__(self):
-        self.tmpl = list()
+        logging.basicConfig(level=logging.DEBUG)
+        self._log = logging.getLogger(__name__)
+
+    def interpret(self, tmpl_path):
+        t = self.load(tmpl_path)
+        if not self.validate_tmpl(t):
+            self._log.error("Parameters defined in Template isn't compatible to OvN")
+        return t
 
     def load(self, tmpl_path):
         f = open(tmpl_path, 'r')
+        t = list()
 
         try:
-            self.tmpl = yaml.load_all(f.read(-1))
+            t.append(i for i in yaml.load_all(f.read(-1)))
         except yaml.YAMLError, exc:
             if hasattr(exc, 'problem_mark'):
                 mark = exc.problem_mark
-            print "Template Format isn't compatible to YAML."
-            print "Error position: (%s:%s)" % (mark.line + 1, mark.column + 1)
+            self._log.error("Template Format isn't compatible to YAML.")
+            self._log.error("Error position: (%s:%s)", mark.line + 1, mark.column + 1)
             exit(-1)
 
-        """
-        Template Validating Process
+        f.close()
+        return t
 
-        Parser Creation Process
-        """
+    def validate(self, ytmpl):
+        for box in ytmpl:
+            if "Box" in box.keys():
+                self._validate_box(box.gets("Box"))
+            else:
+                self._log.warning("Box Part is essential for Provisioning")
+                self._log.warning("The provisioning for box %s will be skipped", )
+                ytmpl.remove(box.gets("Box"))
+                continue
 
-        return self.tmpl
+            if "Bridges" in box.keys():
+                self._validate_box(box.gets("Bridges"))
+                pass
 
-    def validate_tmpl(self, ytmpl):
-        for t in ytmpl:
-            for k in t.keys():
-                if "Box" not in k:
-                    exit(-1)
-
-                if "Bridge" in k:
-                    pass
         return True
+
+    def _validate_box(self, ybox):
+        pass
+
+    def _validate_bridge(self, ybr):
+        pass
+
+    def _validate_flow(self, yfl):
+        pass
 
 
 class BridgeInfoParser:
